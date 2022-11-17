d6bbfcf696a3071c043db692299ef650b8e6f316
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-17 16:55:37 -0500

    added smooth scrolling to navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,12 @@
 import React,{useState,useEffect} from 'react';
+import { Link } from 'react-scroll';
 
 
 function Nav(){
    const links = [
-     { name: 'About', link : '#about'},
-     {name: 'Portfolio' ,link: '#portfolio'},
-     {name: 'Contact', link: '#contact'}
+     { name: 'About', link : 'about'},
+     {name: 'Portfolio' ,link: 'portfolio'},
+     {name: 'Contact', link: 'contact'}
    ] 
 
    const [isScrolled, setIsScrolled] = useState(false);
@@ -33,22 +34,39 @@ function Nav(){
   
 
 
+
    return (
     <header className={`w-full z-50 transition-all duration-300 ease-in ${isScrolled ? 'md:drop-shadow-2xl bg-[#181818]' : 'py-1 '} fixed top-0 w-full block` }>
         <nav className='flex  items-center flex-col w-full  md:flex-row  md:justify-around p-3 md:p-0 '>
             {/* logo */}
             <section className="nav-brand">
-              <a href='#' className=' duration-500 text-white text-4xl hover:text-blue-500 transition-all ' style={styles}>Timi</a>
-
+              <Link
+              className=' duration-500 cursor-pointer text-white text-4xl hover:text-blue-500 transition-all ' 
+              style={styles}
+              spy={true}
+              smooth={true}
+            
+              duration={500}
+              >
+              Timi
+              </Link>
             </section>
             
             {/* nav links */}
             <ul className='flex justify-center items-center md:space-x-7 sm:w-100 p-3 md:p-0'>
-                  {links.map(link => (
-                       <li key={link.name}>
-                         <a href={link.link} className='cursor-pointer p-3 duration-500 hover:text-blue-500'>{link.name}</a>
-                       </li>
-                  ))}
+            {links.map(({name,link}) => (
+                <li key={name}>                   
+                  <Link
+                  className='cursor-pointer p-3 duration-500 hover:text-blue-500'
+                  activeClass="text-blue-500"
+                  to={link} 
+                  spy={true}
+                  smooth={true}
+                  offset={30}
+                  duration={500}
+                >{name}</Link>
+                </li>
+            ))}
             </ul>
             
             {/* Resume Button  */}
