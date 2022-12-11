227dffd2b6a41b5120f1391ef538d69b6322dd39
FOR-TIMI <timiplayskeys@gmail.com>
2022-12-11 11:12:37 -0500

    removed border

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -3,7 +3,7 @@ import heroImage from '../../assets/Images/hero.svg'
 
 function HeroImage() {
   return (
-    <div className=' w-[30%] hidden h-full xl:w-[60%] md:flex justify-center items-center'>
+    <div className=' w-[40%]  hidden h-full xl:w-[60%] md:flex justify-center items-center'>
         <img  className='float-right mt-20' src={heroImage} alt="logo" />
     </div>
   )
